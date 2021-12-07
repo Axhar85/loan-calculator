affb0b11f8e60f1453d0cc31e72b86bc834cf833
Axhar85 <azhararshad_ntu@yahoo.com>
2021-12-07 09:19:44 +0100

    creating loan intrest and rate tabs

--- src/LoanCalculator.tsx
+++ src/LoanCalculator.tsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 export default function LoanCalculator() {
     return (
-        <div>
+        <div className="form-item">
             <h2>Loan Calculator</h2>
-        </div>
+            <div className="form-input"> 
+                <input 
+                    type="number"
+                    name="loan-amount"
+                    placeholder="0"
+                />
+            </div>
+            </div>
+            <div className="form-item">
+            <h2>Intrest Amount</h2>
+            <div className="form-input"> 
+                <input 
+                    type="number"
+                    name="intrest-rate-amount"
+                    placeholder="0"
+                />
+            </div>
+    </div>
+    <div className="form-item">
+    <h2>Loan Terms (years)</h2>
+    <div className="form-input"> 
+        <input 
+            type="number"
+            name="loan-term-amount"
+            placeholder="0"
+        />
+    </div>
+</div>
     )
 };
 
